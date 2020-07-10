ef95553b0ee67e5313e939f570286a6d61c88ff5
Yurii <yurgens87@gmail.com>
2020-07-10 12:56:40 +0300

    footer form script format, error mod-x

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-// Start script for form mail
-$(document).ready(function() {
-  //E-mail Ajax Send
-  $("form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "modx-mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Thank you!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-});
-// END script for form mail
-// start video spinner 
+// start video spinner
 jQuery(document).ready(function (t) {
   t("#play").click(function () {
     t(this)
@@ -26,243 +6,267 @@ jQuery(document).ready(function (t) {
       .html(
         '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div><iframe width="560" height="315" src="https://www.youtube.com/embed/UlOImxSNclQ?autoplay=1" frameborder="0" allowfullscreen></iframe>'
       );
-      //t('#play-video-btn').hide();
+    //t('#play-video-btn').hide();
   });
+  console.log("start footer form");
+  //E-mail Ajax Send
+  $("form").submit(function () {
+    //Change
+    var th = $(this);
+    console.log("до аякса");
+    $.ajax({
+      type: "POST",
+      url: "modx-mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
     });
-//
-jQuery(function(t) {
-      t(window).on("scroll", function() {
-        var e = t(".navbar");
-        e.length &&
-          t(".scrolling-navbar")[
-            e.offset().top > 50 ? "addClass" : "removeClass"
-          ]("top-nav-collapse");
-      });
-    });
-    $(document).ready(function(){
-  $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
-    
-    $(".zoom").hover(function(){
-    
-    $(this).addClass('transition');
-  }, function(){
-        
-    $(this).removeClass('transition');
+    return false;
   });
 });
+//
+jQuery(function (t) {
+  t(window).on("scroll", function () {
+    var e = t(".navbar");
+    e.length &&
+      t(".scrolling-navbar")[e.offset().top > 50 ? "addClass" : "removeClass"](
+        "top-nav-collapse"
+      );
+  });
+});
+$(document).ready(function () {
+  $(".fancybox").fancybox({
+    openEffect: "none",
+    closeEffect: "none"
+  });
+
+  $(".zoom").hover(
+    function () {
+      $(this).addClass("transition");
+    },
+    function () {
+      $(this).removeClass("transition");
+    }
+  );
+});
 function initMap() {
-  if( window.innerWidth >= 768 ){
-      var  pos = {lat: 50.5337356, lng: 30.2188709};
- } else {
-      var  pos = {lat: 50.5335021, lng: 30.2330265}
- } 
-   // var  pos = {lat: 50.5337356, lng: 30.2188709};
-    //var  pos = {lat: 50.5337356, lng: 30.2188709}; position for desctop
-    //var  pos = {lat: 50.5335021, lng: 30.2330265}
-    var opt = {
-      center: pos,
-      zoom: 15,
-      styles: [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#212121"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#212121"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.locality",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#181818"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1b1b1b"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#2c2c2c"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#8a8a8a"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#373737"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#3c3c3c"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#4e4e4e"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#000000"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#3d3d3d"
-      }
-    ]
+  if (window.innerWidth >= 768) {
+    var pos = { lat: 50.5337356, lng: 30.2188709 };
+  } else {
+    var pos = { lat: 50.5335021, lng: 30.2330265 };
   }
-]
-};
-    var myMap = new google.maps.Map(document.getElementById('map'), opt);
-    var marker = new google.maps.Marker({
-      position: {lat: 50.5335021, lng: 30.2330265},
-      map: myMap,
-      title: "Our school",
-    });
-          console.log(marker);
-    var info = new google.maps.InfoWindow({
-      content: '<h3>Tango School</h3>'
-    });
-    marker.addEventListener('click', function(){
+  // var  pos = {lat: 50.5337356, lng: 30.2188709};
+  //var  pos = {lat: 50.5337356, lng: 30.2188709}; position for desctop
+  //var  pos = {lat: 50.5335021, lng: 30.2330265}
+  var opt = {
+    center: pos,
+    zoom: 15,
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#212121"
+          }
+        ]
+      },
+      {
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#212121"
+          }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.country",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9e9e9e"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#bdbdbd"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#181818"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#1b1b1b"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#2c2c2c"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#8a8a8a"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#373737"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#3c3c3c"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#4e4e4e"
+          }
+        ]
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#3d3d3d"
+          }
+        ]
+      }
+    ]
+  };
+  var myMap = new google.maps.Map(document.getElementById("map"), opt);
+  var marker = new google.maps.Marker({
+    position: { lat: 50.5335021, lng: 30.2330265 },
+    map: myMap,
+    title: "Nuestro tango school"
+  });
+  console.log(marker);
+  var info = new google.maps.InfoWindow({
+    content: "<h3>Tango School</h3>"
+  });
+  /*marker.addEventListener('click', function(){
       info.open(myMap, marker);
-    });
-};
+    });*/
+}
+
+/*$(document).ready(function() {
+  
+});*/
